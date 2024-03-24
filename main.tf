203caa9a3bf267c969758db459b6e557219b8a9b
holden.ding <holden.ding@liveramp.com>
2024-03-24 20:13:08 +0800

    feat: add for test

--- main.tf
+++ main.tf
@@ -1,11 +1,5 @@
-
-
 data "alicloud_vpcs" "vpcs_ds" {
-  cidr_block = "10.0.0.0/8"
+  cidr_block = var.cidr_block
   status     = "Available"
   name_regex = "^"
-}
-
-output "first_vpc_id" {
-  value = "${data.alicloud_vpcs.vpcs_ds.vpcs.0}"
 }
--- outputs.tf
+++ outputs.tf
@@ -1,1 +1,3 @@
- 
+output "first_vpc_id" {
+  value = "${data.alicloud_vpcs.vpcs_ds.vpcs.0}"
+}
